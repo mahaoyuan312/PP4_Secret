30e96637554e82859570dc814897ea917bad4633
Haoyuan <hma2@binghamton.edu>
2019-04-09 10:55:45 -0400

    Update app.js
    MongoDB setting up

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //jshint esversion:6
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const ejs = require("ejs");
+const mooose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});

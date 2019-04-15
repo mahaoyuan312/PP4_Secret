fde2fd477ef437fd05522a66491e3debdcb366d9
Haoyuan <hma2@binghamton.edu>
2019-04-15 10:07:47 -0400

    Finished U.I
    U.I:  Home, register and login page, will further polish it in the last week

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
-const mooose = require("mongoose");
+//const mooose = require("mongoose");
 
 const app = express();
 
@@ -12,4 +12,22 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+app.get("/",function(req, res)
+{
+  res.render("home");
+});
+
+app.get("/login",function(req, res) {
+  res.render("login");
+
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.listen(3000, function(){
+console.log("Server started on port:3000");
+
+});

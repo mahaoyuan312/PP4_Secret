cea5b76010dd6a7dfd4294e59a361f101232f4c1
Haoyuan <hma2@binghamton.edu>
2019-04-15 11:00:04 -0400

    User regiser finished
    now an "User" object will be created while users hit register button and the user account informaiton will be save to the local database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
-//const mooose = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,8 +11,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+//connect to the default local port of the mongodb
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+//Schema for the user, pretty much like a structure for object
+const userSchema = {
+ emial : String,
+ password:String
+};
 
-//mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+//create a user using the schema above
+const User = new mongoose.model("User", userSchema);
+
+
 app.get("/",function(req, res)
 {
   res.render("home");
@@ -25,6 +35,22 @@ app.get("/login",function(req, res) {
 
 app.get("/register", function(req, res){
   res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    }else {
+      //we want to render the secretes page until the user has entered the Username and Password
+      res.render("secretes");
+    }
+
+  });
 });
 
 app.listen(3000, function(){

88853e8249ddc7539ab1f5f278228f5ad23a5b8e
Haoyuan <hma2@binghamton.edu>
2019-04-16 11:25:15 -0400

    finished login
    now users can login with their user account created previously

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
-const express = require ("express");
-const bodyParser = require ("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
@@ -12,40 +12,43 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 //connect to the default local port of the mongodb
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-//Schema for the user, pretty much like a structure for object
-const userSchema = {
- emial : String,
- password:String
-};
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
-//create a user using the schema above
-const User = new mongoose.model("User", userSchema);
-
-
-app.get("/",function(req, res)
-{
+//get and response posts sent from html files of different pages
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login",function(req, res) {
+app.get("/login", function(req, res) {
   res.render("login");
 
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
+//Schema for the user, pretty much like a structure for object
+const userSchema = {
+  email: String,
+  password: String
+};
+
+//create a user model using the schema above
+const User = new mongoose.model("User", userSchema);
+
+//render secret page  - register
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password:req.body.password
+    password: req.body.password
   });
-  newUser.save(function (err) {
+  newUser.save(function(err) {
     if (err) {
       console.log(err);
-    }else {
+    } else {
       //we want to render the secretes page until the user has entered the Username and Password
       res.render("secrets");
     }
@@ -53,7 +56,28 @@ app.post("/register", function(req, res){
   });
 });
 
-app.listen(3000, function(){
-console.log("Server started on port:3000");
+//reder secret page - login
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({
+      email: username
+    },
+    function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }
+        }
+      }
+    });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port:3000");
 
 });
